4bd62656a3f73e7329d43d47d557820891aac882
Deonna Hodges <dnhodges@gmail.com>
2017-06-04 23:16:37 -0700

    refactor(deck): Extracted out getSuit().

--- app/src/main/java/co/deonna/solitare/Deck.kt
+++ app/src/main/java/co/deonna/solitare/Deck.kt
@@ -1,17 +1,16 @@
 package co.deonna.solitare
 
 class Deck {
-    
+
     val cards: Array<Card> = Array(52, fun(index: Int): Card {
 
-        val value = index % 13
-        val suit = when (index) {
-            0 -> "Clubs"
-            1 -> "Diamonds"
-            2 -> "Hearts"
-            else -> "Spades"
-        }
-
-        return Card(value, suit)
+        return Card(index % 13, getSuit(index))
     })
+
+    private fun getSuit(index: Int): String = when (index) {
+        0 -> "Clubs"
+        1 -> "Diamonds"
+        2 -> "Hearts"
+        else -> "Spades"
+    }
 }
