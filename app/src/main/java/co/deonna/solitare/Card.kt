47063a63ed5de62f38fb44bd12a275a526339224
Deonna Hodges <dnhodges@gmail.com>
2017-06-04 22:55:41 -0700

    feat(card): Removed Java Card class and simplified Kotlin Card class.

--- app/src/main/java/co/deonna/solitare/Card.kt
+++ app/src/main/java/co/deonna/solitare/Card.kt
@@ -1,10 +1,3 @@
 package co.deonna.solitare
 
-/**
- * Created by deonna on 6/4/17.
- */
-class Card {
-    val value: Int = 0
-    val suit: String = ""
-    var faceUp: Boolean = false
-}
+class Card(val value: Int, val suit: String, var faceUp: Boolean = false)
