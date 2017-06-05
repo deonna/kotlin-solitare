6ec0388e8b789422d4ebc671f2ca6b573e1d7d54
Deonna Hodges <dnhodges@gmail.com>
2017-06-05 14:02:55 -0700

    feat(foundation-pile): Added basic logic for adding, removing and resetting cards in foundation piles and integrated into game model.

--- app/src/main/java/co/deonna/solitare/GameModel.kt
+++ app/src/main/java/co/deonna/solitare/GameModel.kt
@@ -3,4 +3,10 @@ package co.deonna.solitare
 class GameModel {
     val Deck = Deck()
     val wastePile: MutableList<Card> = mutableListOf()
+    val foundationPile: Array<FoundationPile> = arrayOf(
+            FoundationPile("Clubs"),
+            FoundationPile("Diamonds"),
+            FoundationPile("Hearts"),
+            FoundationPile("Spades")
+    )
 }
