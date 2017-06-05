4427adc6c3be6ce9a66621f3dc81e5a925c45c4d
Deonna Hodges <dnhodges@gmail.com>
2017-06-04 23:29:31 -0700

    feat(deck): Added ability to draw a card and reset the entire deck for a new game.

--- app/src/main/java/co/deonna/solitare/Deck.kt
+++ app/src/main/java/co/deonna/solitare/Deck.kt
@@ -1,8 +1,18 @@
 package co.deonna.solitare
 
+import java.util.*
+
 class Deck {
 
     val cards = Array(52, { Card(it % 13, getSuit(it)) })
+    var cardsInDeck = cards.toMutableList();
+
+    fun drawCard(): Card = cardsInDeck.removeAt(0)
+
+    fun reset() {
+        cardsInDeck = cards.toMutableList()
+        Collections.shuffle(cardsInDeck)
+    }
 
     private fun getSuit(index: Int): String = when (index / 13) {
         0 -> "Clubs"
