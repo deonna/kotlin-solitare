195c0feccbe59252480e5d2b86fc58e7aae8bc2e
Deonna Hodges <dnhodges@gmail.com>
2017-06-04 23:20:42 -0700

    fix(deck): Need to divide index by 13.

--- app/src/main/java/co/deonna/solitare/Deck.kt
+++ app/src/main/java/co/deonna/solitare/Deck.kt
@@ -2,9 +2,9 @@ package co.deonna.solitare
 
 class Deck {
 
-    val cards: Array<Card> = Array(52, { index -> Card(index % 13, getSuit(index)) })
+    val cards = Array(52, { index -> Card(index % 13, getSuit(index)) })
 
-    private fun getSuit(index: Int): String = when (index) {
+    private fun getSuit(index: Int): String = when (index / 13) {
         0 -> "Clubs"
         1 -> "Diamonds"
         2 -> "Hearts"
